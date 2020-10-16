d1af3af55a41282370d20ec8967470a917512080
Tacey Wong <xinyong.w@gmail.com>
2020-10-16 13:56:11 +0800

    Update x.py

---
+++ x.py
@@ -0,0 +1,9 @@
+location / {  
+    add_header Access-Control-Allow-Origin *;
+    add_header Access-Control-Allow-Methods 'GET, POST, OPTIONS';
+    add_header Access-Control-Allow-Headers 'DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Authorization';
+
+    if ($request_method = 'OPTIONS') {
+        return 204;
+    }
+} 
