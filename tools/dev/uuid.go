e7290f03f3c7be9982f39e8c04335955c5e76f3f
TaceyWong <xinyong.wang@xtalpi.com>
2020-11-24 18:15:36 +0800

    :delete:移除文件文档

--- tools/dev/uuid.go
+++ tools/dev/uuid.go
@@ -1,11 +1,3 @@
-/*
- * @Description: 填写描述
- * @Author: WangXinyong/TaceyWong
- * @Date: 2020-05-26 18:37:30
- * @LastEditors: WangXinyong/TaceyWong
- * @LastEditTime: 2020-05-26 18:37:37
- * @FilePath: /ctools/tools/dev/uuid.go
- */
 package dev
 
 /*
